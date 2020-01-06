bb1bb7f160df43b4176554965c89f5325bad95a0
Gilles Perreymond <gperreymond@gmail.com>
2020-01-06 15:10:40 +0100

    feat(): nats chart

--- projects/mfaas-cli/services/actions/Install.js
+++ projects/mfaas-cli/services/actions/Install.js
@@ -3,22 +3,26 @@ const path = require('path')
 
 module.exports = {
   handler: async function (ctx) {
-    const { RABBITMQ_VALUES } = ctx.params
+    const { RABBITMQ_VALUES, NATS_VALUES } = ctx.params
     const NAMESPACE = 'moleculerfaas'
     const RABBITMQ_VALUES_PATH = path.resolve(__dirname, '../..', RABBITMQ_VALUES)
+    const NATS_VALUES_PATH = path.resolve(__dirname, '../..', NATS_VALUES)
+    console.log(NATS_VALUES_PATH)
     await this.WriteLogo()
     await sh(`kubectl create namespace ${NAMESPACE}`, true)
     // helm update charts repositories
-    await sh(`
-      helm repo add openfaas https://openfaas.github.io/faas-netes && \
-      helm repo add bitnami https://charts.bitnami.com && \
-      helm repo add ory https://k8s.ory.sh/helm/charts && \
-      helm repo add adwerx https://adwerx.github.io/charts
-    `)
+    await sh('helm repo add bitnami https://charts.bitnami.com')
     // install rabbitmq
     await sh(`
       helm upgrade rabbitmq bitnami/rabbitmq \
       --values ${RABBITMQ_VALUES_PATH} \
+      --namespace ${NAMESPACE} \
+      --install
+    `)
+    // install nats
+    await sh(`
+      helm upgrade nats bitnami/nats \
+      --values ${NATS_VALUES_PATH} \
       --namespace ${NAMESPACE} \
       --install
     `)
