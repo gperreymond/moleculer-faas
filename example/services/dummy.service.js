07fcf16b2ca2548fe428d665593971ebf9a20963
Gilles Perreymond <gperreymond@gmail.com>
2020-01-09 13:46:56 +0100

    feat(): example service dummy

--- example/services/dummy.service.js
+++ example/services/dummy.service.js
@@ -1,13 +1,5 @@
 module.exports = {
   name: 'Dummy',
-  metadata: {
-    aliases: {
-      'POST dummy/hello-world': [
-        'Dummy.HelloWorld'
-      ],
-      'GET dummy/not-hello-world': 'Dummy.NotHelloWorld'
-    }
-  },
   actions: {
     HelloWorld: require('./actions/HelloWorldQuery'),
     NotHelloWorld: require('./actions/NotHelloWorld')
